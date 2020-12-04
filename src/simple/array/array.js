33067dedd477bfccdf346228016b91be40abfc48
W. Guilherme <ravok123@gmail.com>
2020-12-04 18:49:53 -0300

    add: array toContain test

--- src/simple/array/array.js
+++ src/simple/array/array.js
@@ -2,4 +2,9 @@ function retornaPares(array){
    return array.filter(x => x%2 == 0)
 }
 
-module.exports = retornaPares
+function addItem4(array){
+   array.push('item4')
+   return array
+}
+
+module.exports = {retornaPares, addItem4}
--- src/simple/array/array.test.js
+++ src/simple/array/array.test.js
@@ -4,5 +4,11 @@ test('retorna apenas os pares no array', ()=> {
    let array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
    let array2 = [2,4,6,8,10,12,14]
 
-   expect(array(array1)).toEqual(array2)
+   expect(array.retornaPares(array1)).toEqual(array2)
 })
+
+
+test('precisa inserir o item 4 (to contain)', () => {
+   let arraycont = ['item1', 'item2', 'item3']
+   expect(array.addItem4(arraycont)).toContain('item4')
+   })
